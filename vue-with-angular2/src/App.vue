118d838f7a5dd97b8daf352a59f26417628971d5
CHOUAIAR Mohamed-Larbi <m.chouaiar@yahoo.com>
2019-01-02 17:11:37 +0100

    first commit

---
+++ vue-with-angular2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="vue">
+      <img alt="Vue logo" src="assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 10px 60px;
+}
+
+.vue{
+  border-style: solid;
+  border-color: #13521F;
+}
+</style>
---
+++ vue-with-angular2/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <div class="angular">
+    <app-root :number="count" :message="textData" v-on:countChange="handleCountChange"></app-root>
+  </div>
+  <div style="padding: 0px 20px">
+    <md-field>
+      <label>Message</label>
+      <md-input v-model="textData"></md-input>
+    </md-field>
+  </div>
+
+
+  <h3>{{textData}}</h3>
+  <h3>{{count}}</h3>
+
+  <md-button class="md-raised md-primary" v-on:click="increaseCount">Increase the counter</md-button>
+  <md-button class="md-raised md-accent" v-on:click="decreaseCount">Decrease the counter</md-button>
+</div>
+
+
+</template>
+
+<script>
+import Vue from 'vue'
+import { MdButton, MdField } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdButton)
+Vue.use(MdField)
+
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    message: {
+        type: String,
+        default: "Hello this is Vue message"
+    },
+    number:{
+      type: Number,
+        default: 0
+    }
+  },
+   data: function () {
+     return {
+         count:   this.number,
+         textData: this.message
+     }
+   },
+    methods: {
+        increaseCount() {
+            /*let count = */++this.count
+            //this.$emit('counter-updated', count)
+        },
+        decreaseCount() {
+            --this.count
+        },
+        handleCountChange(event){
+            this.count = event.detail
+        }
+    }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.angular{
+  border-style: solid;
+  border-color: #99050a;
+  margin: 30px;
+  padding-bottom: 20px;
+}
+
+</style>
